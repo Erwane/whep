4ac5517ec1d6fe9c7c2911b4e3a58f2ac4788f9e
Erwane <erwane@phea.fr>
2026-07-02 10:31:39 +0200

    Include Mailgun provider in library

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 
+use ResourceHelper\ResourceHelper;
+
 const TESTS = __DIR__ . DIRECTORY_SEPARATOR;
 
 setlocale(LC_ALL, 'C');
+
+ResourceHelper::setBaseDir(__DIR__ . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR);
+
+const TEST_SIGNING_KEY = 'c2da3a65a3a5f47aade89e83fe65b615';
